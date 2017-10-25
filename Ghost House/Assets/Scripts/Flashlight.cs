9e39a4f1490f15335e49c8b3c3b624e1e1e55032
Unknown <devin.j575@stu.nebo.edu>
2017-10-25 13:11:02 -0600

    Added walls and flashlight on/off

---
+++ Ghost House/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	public bool lightOn;
+
+	Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		lightOn = true;
+		light.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.F) && lightOn) {
+			lightOn = false;
+			light.enabled = false;
+		}
+		else if (Input.GetKeyUp (KeyCode.F) && !lightOn){
+			lightOn = true;
+			light.enabled = true;
+		}
+	}
+}
